a41c6df3d22723c2a6705ec5c6b9dcedf2628ddc
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2025-02-17 12:29:21 -0400

    Add project files.

---
+++ church-members-manager.API/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace church_members_manager.API.Models
+{
+    public class Member
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string Names { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string LastNames { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string Role { get; set; }
+        public required string Minister { get; set; }
+        public required string Address { get; set; }
+        public required bool Baptized { get; set; }
+    }
+}
